ecd6d097c8c6642829f4b7b7c5a1750af59d6f06
guzillo <guzillo2077@gmail.com>
2025-09-21 23:22:06 +0200

    add: ultimate recursive method

--- src/markdown.py
+++ src/markdown.py
@@ -111,3 +111,26 @@ def generate_page(from_path, template_path, dest_path):
         os.mkdir(dir_name)
     with open(dest_path, "w") as f:
         f.write(markdown_html)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    files = os.listdir(dir_path_content)
+    for file in files:
+        if os.path.isdir(os.path.join(dir_path_content, file)):
+            if not os.path.exists(os.path.join(dest_dir_path, file)):
+                os.mkdir(os.path.join(dest_dir_path, file))
+            generate_pages_recursive(
+                os.path.join(dir_path_content, file),
+                template_path,
+                os.path.join(dest_dir_path, file),
+            )
+            continue
+        one = os.path.join(os.path.abspath(dir_path_content), file)
+        two = os.path.join(os.path.abspath(dest_dir_path), file)
+        print(f"file:{one} --> file:{two}")
+        html_file_name = file[::-1].split(".", 1)[1][::-1] + ".html"
+        generate_page(
+            os.path.join(os.path.abspath(dir_path_content), file),
+            template_path,
+            os.path.join(os.path.abspath(dest_dir_path), html_file_name),
+        )
